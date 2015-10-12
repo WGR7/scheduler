e8f2e3b51ee57435d0a32e6a3129350208537aee
gshrikant <shrikantgiridhar@gmail.com>
2015-10-13 00:47:57 +0530

    Adds basic context switch logic
    This adds a basic context switch routine with rough tasks.
    The context switch doesn't do anything right now, just starts one
    hardcoded task.
    
    Also adds skeleton structures for tasks.

--- src/main.c
+++ src/main.c
@@ -4,38 +4,31 @@
 #include <stdbool.h>
 
 #include <lm3s811.h>
-// #include "task.h"
+#include "task.h"
 
 #define TICK_FREQ       100
 #define TICK_PERIOD     (1/TICK_FREQ)
 
 #define __DEBUG__
 
+#define TASK_A_SP       (uint32_t*)(0x20000200)
+#define TASK_B_SP       (uint32_t*)(0x20000100)
+
+
 void hw_init();
 
 void LED_On(void);
 void LED_Off(void);
 
-volatile uint8_t flag;
-
-void (*volatile LED_fn)(void);
-
 void SysTick_Handler(void) __attribute__((naked));
-
 
 int main(void)
 {
+    create_task(&LED_On, TASK_A_SP);
+    create_task(&LED_Off, TASK_B_SP);
     hw_init();
-#ifndef __DEBUG__
-    sched_init();
-    create_task(&LED_On, 0);
-    create_task(&LED_Off, 1);
-    sched_start();
-#endif
-    flag = 0;
-    LED_fn = &LED_On;
     while(1) {
-        LED_fn();
+        LED_Off();
     }
 }
 
@@ -57,30 +50,25 @@ void hw_init(void)
 
 void SysTick_Handler(void)
 {
-    if (flag) {
-        // When we do enter here, we'll have a functioning stack
-        // So just get the stack pointer
-        __asm__ volatile("push {r4-r7}\r\n");
-    }
-
-    if (!flag) {
-        flag ^= 0x01;
-        LED_fn = &LED_Off;
-    }
     // Store the registers not already stored by NVIC
-    // r4-r7
-    // Since Thumb code can access only the lower registers
-    // Decrement `sp` before storing the values
-    // Implement a simpler version where we begin with already
-    // executing tasks - so the stacks are already setup
+    __asm__ volatile ("nop \r\n"
+                      "ldr sp, =0x200001c0 \r\n"
+                      "pop {r4-r11} \r\n"
+                      "bx lr \r\n");
 }
 
 void LED_On(void)
 {
     GPIO_PORTC_DATA_R |= (1U << 5);
+    while(1) {
+        __asm__ volatile("nop");
+    }
 }
 
 void LED_Off(void)
 {
-   GPIO_PORTC_DATA_R &= ~(1U << 5);
+    GPIO_PORTC_DATA_R &= ~(1U << 5);
+    while(1) {
+        __asm__ volatile("nop");
+    }
 }
--- src/startup.c
+++ src/startup.c
@@ -45,7 +45,7 @@ extern int main(void);
 // Reserve space for the system stack.
 //
 //*****************************************************************************
-static unsigned long pulStack[64];
+static unsigned long pulStack[256];
 
 //*****************************************************************************
 //
